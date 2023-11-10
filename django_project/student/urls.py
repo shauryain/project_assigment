c5f03597037e0f9e9ff62d455804261bbb5bc049
shauryain <147681669+shauryain@users.noreply.github.com>
2023-11-10 19:21:11 +0530

    form
    fill out the form for further process.

--- django_project/student/urls.py
+++ django_project/student/urls.py
@@ -5,5 +5,6 @@ from .import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",views.index),
-    path("<int:id>",views.view_student,name="view_student") #path convert that allows  create daynamin url int matches integer  when djanog  is pregenting with urls mathcing this pattern for example /5 django puts integer 5 into the variable(give id 5 your id is matche id gives us id 5 relted think)
+    path("<int:id>",views.view_student,name="view_student") ,#path convert that allows  create daynamin url int matches integer  when djanog  is pregenting with urls mathcing this pattern for example /5 django puts integer 5 into the variable(give id 5 your id is matche id gives us id 5 relted think)
+    path("add/",views.add,name="add")
 ]
