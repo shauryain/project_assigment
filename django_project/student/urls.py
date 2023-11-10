f3500427950250fb61893d1b233bac1db42c35ea
shauryain <147681669+shauryain@users.noreply.github.com>
2023-11-10 16:43:21 +0530

    icon is add(other stuff)
    inside html page icon is and migration is completed

--- django_project/student/urls.py
+++ django_project/student/urls.py
@@ -4,5 +4,6 @@ from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",views.index)
+    path("",views.index),
+    path("<int:id>",views.view_student,name="view_student") #path convert that allows  create daynamin url int matches integer  when djanog  is pregenting with urls mathcing this pattern for example /5 django puts integer 5 into the variable(give id 5 your id is matche id gives us id 5 relted think)
 ]
--- django_project/student/views.py
+++ django_project/student/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from .models import Student
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 
 def index(request):
     return render(request,"student/index.html",{
-        "student":Student.objects.all()
+        "students":Student.objects.all()
     })
+
+
+def view_student(request,id):
+    student=Student.objects.get(pk=id)
+
+    return HttpResponseRedirect(reverse("index"))
+
+
+
